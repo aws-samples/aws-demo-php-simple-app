844e55089d0de323e07e82cd6d7325a1fc5d04c6
aws-joe <fontesj@amazon.com>
2015-06-18 16:59:51 -0700

    index updates on redirect

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-$outBound = "http://".$_SERVER['SERVER_NAME']."/www/";
+$dirVal = dirname($_SERVER['PHP_SELF']);
+
+if(strlen($dirVal) > 1)
+	$outBound = "http://".$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF'])."/www/";
+else
+	$outBound = "http://".$_SERVER['SERVER_NAME']."/www/";
 
 header("Location: $outBound");
 
