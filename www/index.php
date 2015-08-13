53e430b8f23113d0c30c2c42bdba1eb5deb74d11
aws-joe <fontesj@amazon.com>
2015-08-12 17:14:03 -0700

    removed unused fonts

--- www/index.php
+++ www/index.php
@@ -35,7 +35,6 @@ include "lib.php";
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
-	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="css/templatemo_style.css" rel="stylesheet" type="text/css">
 	<script src="Chart.min.js"></script>
