ed8639ba6c1f6aa88cd2e8fc696ad20c400ec252
aws-joe <fontesj@amazon.com>
2017-01-09 20:56:35 +0000

    added comments as well as top description and licensing info

--- solano-test.sh
+++ solano-test.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+#
+#       http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+#
+# Description: Single bash script to run Solano CI tests.  Can add more tests below as necessary.
 
 sudo docker run ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${DOCKER_APP}:${TDDIUM_SESSION_ID} /usr/bin/php /var/www/html/www/index.php
 EXIT_CODE=$?
